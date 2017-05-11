847f278cccd760c1ffebdaee4d1e5a2abdcc2307
Chris Harrold <christophereharrold@hotmail.com>
2017-05-11 15:49:56 -0700

    First commit of new code for photon
    working prototype code for reading a single soil moisture probe with a
    particle photon on a 10 second loop

---
+++ SoilMoisture/src/SoilMoisture.ino
@@ -0,0 +1,59 @@
+
+// ------------
+// Initial Soil Moisture Read Code - Single Sensor with LED indicators
+// ------------
+
+/*-------------
+
+This simple flash will do two things:
+
+Turn on an LED to indicate that the Device is in the active monitoring state
+
+Take a reading from the YL-69 Soil Moisture probe analog pin
+
+-------------*/
+
+int aled = D0; // this is the activity LED (aled)
+int ppin = D1; // This is the pin that will turn on the YL-69 Sensor
+int arpin = A0; // This is the pin we will take the analog value reading from
+
+void setup() {
+
+  // Set the LED and power pins to output mode
+  // It's important you do this here, inside the setup() function rather than
+  // outside it or in the loop function.
+
+  pinMode(aled, OUTPUT);
+  pinMode(ppin, OUTPUT);
+  // read in a code snipit note that you DO NOT DO THIS: pinMode(arpin, INPUT);
+
+}
+
+// Next we have the loop function, the other essential part of a microcontroller program.
+
+void loop() {
+  // here we will start the process by turning on the indicator LED (aLED)
+  digitalWrite(aled, HIGH);
+
+  // Next we turn on the power to the sensor
+  digitalWrite(ppin, HIGH);
+
+  // attempting to read immediately causes funky voltages so we will wait for 1 second
+  // before we poll the analog pin
+  delay(3000);
+
+  // Now we will pull the votage value from the probe
+  int SMVolts = analogRead(arpin);
+  // and use the particle.publish event trigger to see what the value is via the
+  // particle build console
+  Particle.publish("SoilVolts", String(SMVolts));
+
+  // Then we'll turn it off...
+  digitalWrite(ppin, LOW);
+  digitalWrite(aled, LOW);
+
+  // Wait 10 seconds for the sake of debugging...
+  delay(10000);
+
+  // And repeat!
+}
