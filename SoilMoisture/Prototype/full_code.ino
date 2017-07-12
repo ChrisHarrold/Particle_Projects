2d0ffbe33ac551aea31f6850f6bd6d52bc7ce622
Chris Harrold <christophereharrold@hotmail.com>
2017-07-12 16:43:44 -0600

    checkpoint save
    Most functionality implemented and working
    
    - debugger error on the snprintf function
    - simplified pin power-on and off
    - better comments and functions
    - removed Herobrine

--- SoilMoisture/Prototype/full_code.ino
+++ SoilMoisture/Prototype/full_code.ino
@@ -13,30 +13,10 @@ STARTUP(System.enableFeature(FEATURE_RETAINED_MEMORY));
 int aled = D4; // this is the activity LED (aled) - it comes on when a sensor is sampling
 int pled = D5; // This LED will come on when the photon is offline from cloud
 
-// Since this is my prototype the pin to sensor mapping kinda sucks because
-// I have never designed a PCB before, but this is what happens when you marry
-// hardware and software
 
-// power for S0
-int ps0 = D3;
-
-// power for S1
-int ps1 = D2;
-
-// power S2
-int ps2 = D1;
-
-// power S3
-int ps3 = D0;
-
-// power S4
-int ps4 = D6;
-
-// power S5
-int ps5 = D7;
-
-// placeholder for the analog probe reading pins
+// placeholder for the analog probe reading pins and digital power pins
 int as[6];
+int ps[6];
 
 // what is this thing called? Set in the "setup" section below
 retained char dev_name[12];
@@ -46,24 +26,13 @@ retained char offlinevals[1024];
 retained int16_t globalindex = 0;
 // is the particle online value check
 int onlinechk;
-// set this value to the # of miliseconds between polling intervals
-// easy ref table:
-//
-// 2 hours = 7200000
-// 6 hours = 21600000
-// 12 hours = 43200000
-// 24 = 86400000
-// I would recommend values basd on climate - longer for wet and shorter for
-// hot and dry
-int poll_delay = 10000;
 // this is a simple int variable for loops and counters
 int a = 0;
 
 void setup() {
 
   // I have 6 analog pins to read for the moisture probes - this tells
-  // the program which pins to use and must match the PCB (this is the map for
-  // the prototype boards)
+  // the program which pins to use and must match the PCB (prototype map)
   as[0] = A1;
   as[1] = A2;
   as[2] = A3;
@@ -71,21 +40,28 @@ void setup() {
   as[4] = A5;
   as[5] = A6;
 
+  // mapping for the digital pins is not quite as clean as the analog's, but
+  // once done here I can use the same for loops to process turning them on
+  // and off like I do with the analog pins for reading
+  ps[0] = D3;
+  ps[1] = D2;
+  ps[2] = D1;
+  ps[3] = D0;
+  ps[4] = D6;
+  ps[5] = D7;
+
 
   // Set the LED and power pins to output mode
-  // It's important you do this here, inside the setup() function rather than
-  // outside it or in the loop function.
 
   pinMode(pled, OUTPUT);
   pinMode(aled, OUTPUT);
-  pinMode(ps0, OUTPUT);
-  pinMode(ps1, OUTPUT);
-  pinMode(ps2, OUTPUT);
-  pinMode(ps3, OUTPUT);
-  pinMode(ps4, OUTPUT);
-  pinMode(ps5, OUTPUT);
+  for(a = 0; a < 6; a = a + 1 ) {
+    pinMode(ps[a], OUTPUT);
+  }
 
-  snprintf(dev_name, sizeof(dev_name), "%s", "Tomato_1");
+  // Change "Tomato_1" to whatever you want the device to be - this will happen
+  // during setup at some point, but it gets manual treatment for now
+  strcpy(dev_name, "Tomato_1");
 
 }
 
@@ -98,26 +74,56 @@ void onlinepoll() {
     //be used in later steps
     time_t time = Time.now();
     String timestamp =  String(Time.timeStr());
+    publishdata = timestamp + "::";
 
-
+    // For loop takes a reading from each analog pin and creates the output String
+    // to send to the particle.publish function - this will eventually change to
+    // being sent to the base-station, but for now it's good to see results
     for(a = 0; a < 6; a = a + 1 ) {
       int SMVolts = analogRead(as[a]);
         if (SMVolts < 4000) {
           String voltage = String(SMVolts);
-          String publishdata = publishdata + "S" + a + "::" + timestamp + "::" + voltage + "::";
+          String publishdata = publishdata + "S" + a + "::" + voltage + "::";
 
         }
         else {
-          String publishdata = publishdata + "S" + a + "::No Sensor discovered!::";
+          String publishdata = publishdata + "S" + a + "::DC::";
         }
     }
-
+    // Publish the readings to the cloud
     Particle.publish(dev_name, publishdata, PRIVATE);
 }
 
 void offlinepoll() {
 
+  // Since the photon is offline we have to save the values to upload when
+  // we get a connection - same starting cleanup and prep though
+  String publishdata = "";
+  time_t time = Time.now();
+  String timestamp =  String(Time.timeStr());
+  publishdata = timestamp + "::";
 
+  // For loop takes a reading from each analog pin and creates the output String
+  // to send to the char array for storing until connections resume
+  for(a = 0; a < 6; a = a + 1 ) {
+    int SMVolts = analogRead(as[a]);
+      if (SMVolts < 4000) {
+        String voltage = String(SMVolts);
+        String publishdata = publishdata + "S" + a + "::" + voltage + "::";
+      }
+      else {
+        String publishdata = publishdata + "S" + a + "::DC::";
+      }
+  }
+  // store the reading in the array
+  snprintf(offlinevals, sizeof(offlinevals), "%s", publishdata);
+}
+
+void update_now() {
+
+  snprintf(offlinevals, sizeof(offlinevals), "%s", "data to sprintf");
+  Particle.publish("SMP_Update", offlinevals, PRIVATE);
+  delay(1000);
 }
 
 // Next we have the loop function, the main part of the program.
@@ -131,9 +137,7 @@ void loop() {
     // if so, then check to see if the photon is online
     if (Particle.connected()) {
       //If so, upload the data stored in the offlinevals array for the dashboard
-      snprintf(offlinevals, sizeof(offlinevals), "%s", "data to sprintf");
-      Particle.publish("SMP_Update", offlinevals, PRIVATE);
-      delay(1000);
+      update_now();
       onlinechk = 1;
       loops = 0;
     }
@@ -144,12 +148,9 @@ void loop() {
   }
 
   // Next we turn on the power to the sensor
-  digitalWrite(ps0, HIGH);
-  digitalWrite(ps1, HIGH);
-  digitalWrite(ps2, HIGH);
-  digitalWrite(ps3, HIGH);
-  digitalWrite(ps4, HIGH);
-  digitalWrite(ps5, HIGH);
+  for(a = 0; a < 6; a = a + 1 ) {
+    digitalWrite(ps[a], HIGH);
+  }
   // attempting to read immediately causes funky voltages so we will wait for 2
   // seconds before we poll the analog pin
   delay(2000);
@@ -163,18 +164,26 @@ void loop() {
   }
 
   // Then we'll turn off the sensor pack
-  digitalWrite(ps0, LOW);
-  digitalWrite(ps1, LOW);
-  digitalWrite(ps2, LOW);
-  digitalWrite(ps3, LOW);
-  digitalWrite(ps4, LOW);
-  digitalWrite(ps5, LOW);
+  for(a = 0; a < 6; a = a + 1 ) {
+    digitalWrite(ps[a], LOW);
+  }
 
   // Turn off the activity LED and then
-  // wait delay seconds for the sake of debugging...
+  // put the system to sleep for (poll_delay) seconds
+  // defaulted to 10 for the sake of debugging speed...
   loops = loops + 1;
   digitalWrite(aled, LOW);
-  delay(poll_delay);
+  // set this value to the # of seconds between polling intervals
+  // easy ref table:
+  //
+  // 2 hours = 7200
+  // 6 hours = 21600
+  // 12 hours = 43200
+  // 24 = 86400
+  //
+  // I would recommend values basd on climate - longer for wet and shorter for
+  // hot and dry - this puts the photon to sleep so it uses less juice
+  System.sleep(10);
 
   // And repeat!
 }
